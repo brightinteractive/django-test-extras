88899aec147fa1596945acedbb9d18b34019dc22
Francis Devereux <francis@bright-interactive.co.uk>
2012-08-10 14:05:03 +0100

    Add DataPreservingTransactionTestCaseMixin

--- test_extras/testcases.py
+++ test_extras/testcases.py
@@ -2,8 +2,14 @@
 # (c) 2012 Bright Interactive Limited. All rights reserved.
 # http://www.bright-interactive.com | info@bright-interactive.com
 
+from StringIO import StringIO
 from django.core import management
+from django.core import serializers
+from django.core.management import call_command
 from django.core.management.commands import flush
+from django.db import transaction, connection, connections, DEFAULT_DB_ALIAS, reset_queries, router
+import os
+import tempfile
 
 
 class SkipFlushCommand(flush.Command):
@@ -16,8 +22,9 @@ class NonFlushingTransactionTestCaseMixin(object):
     Mixin that prevents the database from being flushed in TransactionTestCase
      subclasses.
 
-    If you mix this in then your tests must delete any data that they create,
-    instead of relying on TransactionTestCase's flushing to do it for then.
+    Only suitable for tests that don't change any database data - if your test
+    does change the database then use DataPreservingTransactionTestCaseMixin
+    instead.
 
     Only works if you mix it in first like this:
        class MyTestCase(NonFlushingTransactionTestCaseMixin, TransactionTestCase):
@@ -38,3 +45,44 @@ class NonFlushingTransactionTestCaseMixin(object):
         finally:
             # unpatch flush back to the original
             management._commands['flush'] = original_flush_command
+
+
+class DataPreservingTransactionTestCaseMixin(NonFlushingTransactionTestCaseMixin):
+    """
+    Mixin that prevents the database from being flushed in TransactionTestCase
+     subclasses and that saves the database contents before each test and
+     restores it afterwards, so it is suitable for use with tests that update
+     the database.
+
+    Only works if you mix it in first like this:
+       class MyTestCase(DataPreservingTransactionTestCaseMixin, TransactionTestCase):
+    not like this:
+       class MyTestCase(TransactionTestCase, DataPreservingTransactionTestCaseMixin):
+    """
+
+    def setUp(self):
+        super(DataPreservingTransactionTestCaseMixin, self).setUp()
+
+        self.database_dumpfilepaths = {}
+
+        # If the test case has a multi_db=True flag, back up all databases.
+        # Otherwise, just use default.
+        # This mirrors TransactionTestCase's behaviour
+        if getattr(self, 'multi_db', False):
+            databases = connections
+        else:
+            databases = [DEFAULT_DB_ALIAS]
+        for db in databases:
+            with tempfile.NamedTemporaryFile(suffix='.json', delete=False) as dumpfile:
+                call_command('dumpdata',
+                    verbosity=0, interactive=False,
+                    database=db, use_base_manager=True, format='json', stdout=dumpfile)
+            self.database_dumpfilepaths[db] = dumpfile.name
+
+    def tearDown(self):
+        super(DataPreservingTransactionTestCaseMixin, self).tearDown()
+
+        for (db, dumpfilepath) in self.database_dumpfilepaths.iteritems():
+            call_command('superflush', verbosity=0, interactive=False, database=db)
+            call_command('loaddata', dumpfilepath, verbosity=0, interactive=False)
+            os.remove(dumpfilepath)
