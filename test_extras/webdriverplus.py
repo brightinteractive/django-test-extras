e79ecbd95f980ee13986fdbcccac70c99877c823
Francis Devereux <francis@bright-interactive.co.uk>
2012-09-03 16:21:16 +0100

    Add a few utility methods to WebDriverPlusTestCase:
    find_and_wait()
    find_hidden_text()
    get_hidden_text()

--- test_extras/webdriverplus.py
+++ test_extras/webdriverplus.py
@@ -11,9 +11,11 @@ have to depend on selenium or webdriverplus.
 
 from __future__ import absolute_import
 
+from bs4 import BeautifulSoup
 from django.conf import settings
-from webdriverplus import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
 from test_extras.testcases import DataPreservingTransactionTestCaseMixin
+from webdriverplus import WebDriver
 import django.test
 
 # Default to Firefox but allow this to be overridden in Django settings
@@ -21,6 +23,10 @@ browser = getattr(settings, 'WEBDRIVERPLUS_BROWSER', 'firefox')
 
 
 class WebDriverPlusTestCase(DataPreservingTransactionTestCaseMixin, django.test.LiveServerTestCase):
+    # How long to wait for pages to load or elements to become available.
+    # Can be overridden by subclasses (not tested yet!).
+    PAGE_TIMEOUT_SECONDS = 10
+
     @classmethod
     def setUpClass(cls):
         cls.browser = WebDriver(browser=browser)
@@ -34,3 +40,24 @@ class WebDriverPlusTestCase(DataPreservingTransactionTestCaseMixin, django.test.
         # tearDownClass first.
         cls.browser.quit()
         super(WebDriverPlusTestCase, cls).tearDownClass()
+
+    def find_and_wait(self, *args, **kwargs):
+        return WebDriverWait(self.browser, self.PAGE_TIMEOUT_SECONDS).until(
+            lambda browser: browser.find(*args, **kwargs)
+            )
+
+    def find_hidden_text(self, *args, **kwargs):
+        element = self.browser.find(*args, **kwargs)
+        return self.get_hidden_text(element)
+
+    def get_hidden_text(self, element):
+        """
+        Like element.text, but doesn't just return an empty string if the
+        element is hidden.
+        """
+
+        # element.text will work if the element is visible, but if it or
+        # any of its parents are hidden it won't, so we resort to parsing the
+        # HTML
+        soup = BeautifulSoup(element.html)
+        return soup.text
