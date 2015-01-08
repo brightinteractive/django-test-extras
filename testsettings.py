741ebb9c3f5fc12b11568da171df4a08e412e895
Francis Devereux <francis@bright-interactive.co.uk>
2015-01-08 16:28:12 +0000

    Fix Django 1.7 warning "(1_7.W001) MIDDLEWARE_CLASSES is not set"

--- testsettings.py
+++ testsettings.py
@@ -13,6 +13,16 @@ INSTALLED_APPS = (
     'test_app',
 )
 
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
 # Django 1.7's built-in migrations framework supersedes South
 # See http://south.readthedocs.org/en/latest/releasenotes/1.0.html#library-migration-path
 _current_version = NormalizedVersion(django.get_version())
